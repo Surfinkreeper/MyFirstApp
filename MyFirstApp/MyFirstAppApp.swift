fec945fa3bdb3b2ca0faae67e15ee06393e219a3
Surfinkreeper <danielsalameh04@gmail.com>
2024-08-11 23:33:00 -0700

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Daniel Salameh on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
